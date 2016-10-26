39edad3369ea32cd4ba5fcdca0c77a0116eb559e
yuzai <a545905403@163.com>
2016-10-26 21:56:28 +0800

    a robot chat software, just for fun

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function get_jsonp(text) {
+    $.getJSON("http://op.juhe.cn/robot/index?callback=?", {
+        "info" : text,
+        "dtype" : "json",
+        "key" : "5886c0226172025122705c2d5a3f1278",
+        "userid":111
+    }, show);
+ }
+ $('#info').keyup(function(e){
+         var key =  e.which;
+         if(key == 13)
+           $('#send').trigger("click");
+     });
+ $('#send').click(function(){
+   var text = $('#info').val();
+   $('#info').val('');
+   var p = "<p style = 'text-align:right'>"+text+'</p>';
+   $('#chat').append(p);
+   $('#chat').scrollTop( $('#chat')[0].scrollHeight );
+    get_jsonp(text);
+ })
+ function show(data){
+   var p = "<p  style = 'text-align:left'>"+data.result.text+'</p>';
+   $('#chat').append(p);
+   $('#chat').scrollTop( $('#chat')[0].scrollHeight );
+ }
